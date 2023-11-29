2244bd03a4a50037233a04e81181f54d45c9171a
Kat-Wang <katewang08@gmail.com>
2023-11-28 19:09:44 -0600

    made the supertype filter button into a radio button

--- pokemon-tcg-inventory/Views/CardsView.swift
+++ pokemon-tcg-inventory/Views/CardsView.swift
@@ -13,54 +13,17 @@ let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridIte
 struct CardsView: View {
     
     @State var cardFilters = CardFilters()
-    @State var pokemonSubtypeSegCtl = 0
     
     var body: some View {
         
         VStack {
             HStack {
-                FilterButton(text: "Pokemon", filter: "pokemon", cardFilters: cardFilters)
-                FilterButton(text: "Trainer", filter: "trainer", cardFilters: cardFilters)
-                FilterButton(text: "Energy", filter: "energy", cardFilters: cardFilters)
+                SupertypeFilterButton(text: "Pokemon", filter: "pokemon", cardFilters: $cardFilters)
+                SupertypeFilterButton(text: "Trainer", filter: "trainer", cardFilters: $cardFilters)
+                SupertypeFilterButton(text: "Energy", filter: "energy", cardFilters: $cardFilters)
             }
-            
-            Picker("Select a Pokemon subtype", selection: $pokemonSubtypeSegCtl) {
-                Text("Stage").tag(0)
-                Text("Mechanic").tag(1)
-                Text("Type").tag(2)
-                Text("Label").tag(3)
-                Text("Extra").tag(4)
-            }
-            .pickerStyle(.segmented)
-            .frame(width: 350, height: 25)
-            .padding(.top, 10)
-            
-            RoundedRectangle(cornerRadius: 10)
-                .foregroundColor(.white)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 2)
-                )
-                .overlay(
-                    segCtrlfilterButtonGrid
-                )
-                .frame(width: 350, height: 150)
+            PokemonSubTypesFilters(cardFilters: cardFilters)
         }
-    }
-    
-    @ViewBuilder
-    var segCtrlfilterButtonGrid: some View {
-        switch pokemonSubtypeSegCtl {
-                case 0:
-                    FilterButtonGrid(filters: cardFilters.stageFilters, filterNames: cardFilters.stageFilterNames, cardFilters: cardFilters)
-                case 1:
-                    FilterButtonGrid(filters: cardFilters.mechanicFilters, filterNames: cardFilters.mechanicFilterNames, cardFilters: cardFilters)
-                case 2:
-                    FilterButtonGrid(filters: cardFilters.typeFilters, filterNames: cardFilters.typeFilterNames, cardFilters: cardFilters)
-                case 3:
-                    FilterButtonGrid(filters: cardFilters.labelFilters, filterNames: cardFilters.labelFilterNames, cardFilters: cardFilters)
-                default:
-                    FilterButtonGrid(filters: cardFilters.extraFilters, filterNames: cardFilters.extraFilterNames, cardFilters: cardFilters)
-                }
     }
 }
 
@@ -85,3 +48,51 @@ struct FilterButtonGrid: View {
     }
 }
 
+struct PokemonSubTypesFilters: View {
+
+    var cardFilters: CardFilters
+    
+    @State var pokemonSubtypeSegCtl = 0
+    
+    var body: some View {
+        VStack {
+            Picker("Select a Pokemon subtype", selection: $pokemonSubtypeSegCtl) {
+                Text("Stage").tag(0)
+                Text("Mechanic").tag(1)
+                Text("Type").tag(2)
+                Text("Label").tag(3)
+                Text("Extra").tag(4)
+            }
+            .pickerStyle(.segmented)
+            .frame(width: 350, height: 25)
+            .padding(.top, 10)
+            
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 2)
+                )
+                .overlay(
+                    segCtrlfilterButtonGrid
+                )
+                .frame(width: 350, height: 150)
+        }
+        }
+    
+    @ViewBuilder
+    var segCtrlfilterButtonGrid: some View {
+        switch pokemonSubtypeSegCtl {
+        case 0:
+            FilterButtonGrid(filters: cardFilters.stageFilters, filterNames: cardFilters.stageFilterNames, cardFilters: cardFilters)
+        case 1:
+            FilterButtonGrid(filters: cardFilters.mechanicFilters, filterNames: cardFilters.mechanicFilterNames, cardFilters: cardFilters)
+        case 2:
+            FilterButtonGrid(filters: cardFilters.typeFilters, filterNames: cardFilters.typeFilterNames, cardFilters: cardFilters)
+        case 3:
+            FilterButtonGrid(filters: cardFilters.labelFilters, filterNames: cardFilters.labelFilterNames, cardFilters: cardFilters)
+        default:
+            FilterButtonGrid(filters: cardFilters.extraFilters, filterNames: cardFilters.extraFilterNames, cardFilters: cardFilters)
+        }
+    }
+    
+}
--- pokemon-tcg-inventory/Views/Subviews/FilterButton.swift
+++ pokemon-tcg-inventory/Views/Subviews/FilterButton.swift
@@ -11,14 +11,57 @@ struct FilterButton: View {
     
     var text: String
     var filter: String
-    
     var cardFilters: CardFilters
     
     var body: some View {
         
         Button {
             cardFilters.filters[filter] = !cardFilters.filters[filter]!
-            print(cardFilters.filters[filter]! as Bool)
+        } label: {
+            Text(text)
+                .font(.caption)
+                .frame(width: 80, height: 35)
+                .foregroundColor(cardFilters.filters[filter]! ? .white : .black)
+                .background(RoundedRectangle(
+                    cornerRadius: 20,
+                    style: .continuous
+                )
+                    .fill(cardFilters.filters[filter]! ? Color(hex: "#4484b2") : .white)
+                )
+                .overlay {
+                    RoundedRectangle(
+                        cornerRadius: 20,
+                        style: .continuous
+                    )
+                    .stroke(cardFilters.filters[filter]! ? Color(hex: "#4484b2") : .gray, lineWidth: 2)
+                }
+        }
+    }
+}
+
+//TODO: Create a filter button specific to supertypes
+struct SupertypeFilterButton: View {
+    
+    var text: String
+    var filter: String
+    
+    @Binding var cardFilters: CardFilters
+    
+    var body: some View {
+        Button {
+            if filter == "pokemon" {
+                cardFilters.filters["trainer"] = false
+                cardFilters.filters["energy"] = false
+                print("hello")
+            } else if filter == "trainer" {
+                cardFilters.filters["pokemon"] = false
+                cardFilters.filters["energy"] = false
+            } else if filter == "energy" {
+                cardFilters.filters["pokemon"] = false
+                cardFilters.filters["trainer"] = false
+            }
+            
+            cardFilters.filters[filter] = !cardFilters.filters[filter]!
         } label: {
             Text(text)
                 .font(.caption)
